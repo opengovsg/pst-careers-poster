236003dfa42f44ed2bfadc7776943d8ef1d40ad5
LoneRifle <LoneRifle@users.noreply.github.com>
2026-03-21 21:30:39 +0800

    chore: drop support for Langfuse

--- workflows/shared/model.ts
+++ workflows/shared/model.ts
@@ -1,16 +1,9 @@
 import { createOpenAI } from '@ai-sdk/openai'
-import { observeOpenAI } from '@langfuse/openai'
 
-const isLangfuseActive =  process.env.LANGFUSE_SECRET_KEY && process.env.LANGFUSE_PUBLIC_KEY
-
-const addInstrumentation: typeof observeOpenAI = isLangfuseActive 
-  ? observeOpenAI
-  : (provider) => provider
-
-export const provider = addInstrumentation(createOpenAI({
+export const provider = createOpenAI({
   name: 'engine',
   baseURL: process.env.OPENAI_ENDPOINT!,
   apiKey: process.env.OPENAI_API_KEY!,
-}))
+})
 
 export const model = provider(process.env.OPENAI_MODEL_NAME!)
